ef34b9db5cefedf0f0e28783354f73de3f0ccb42
Robert <varken@robert.com>
2020-06-25 15:22:04 +0200

    Eerste idee voor een frontend

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app color="#111111" dark>
+      <div class="d-flex align-center" style="padding-right: 20px; color:#d0d0d0; ">Threat Hunting</div>
+        <div class="d-flex align-center" style="padding-right: 20px; color:#d0d0d0;">Protocols</div>
+      <div class="d-flex align-center" style="padding-right: 20px; color:#d0d0d0;">Write plugins</div>
+      <div class="d-flex align-center" style="padding-right: 20px; color:#d0d0d0;">Tutorial</div>
+      <v-spacer></v-spacer>
+      <div class="d-flex align-center" style="padding-right: 20px; color:#d0d0d0;">Log in</div>
+    </v-app-bar>
+
+    <v-content style="background-color: #111111;">
+      <Portal />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Portal from "./components/Portal";
+
+export default {
+  name: "App",
+
+  components: {
+    Portal
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/components/Portal.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-row justify="center" >
+      <v-col cols="12" sm="1" class="d-flex justify-center">
+        <h1 style="color:#488ed2; font-size:50px; font-weight: 400">NETLOG</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="3" class="d-flex justify-center">
+        <center><p style="color:#d0d0d0">An extensible framework for forensic network analysts. Customize Netlog to find what you are looking for.</p></center>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="4">
+        <center>
+          <div class="mycontainer"></div>
+        </center>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="4">
+        <center>
+          <br>
+          <v-btn
+            rounded
+            style="background-color: #111111; color: #488ed2; border: 2pt solid #488ed2"
+          >Analyze pcap file</v-btn>
+        </center>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Portal",
+  data: () => ({})
+};
+</script>
+<style scoped lang="scss">
+.mycontainer {
+  z-index: 2;
+  background-image: radial-gradient(closest-side, #11111100 70%, #111111ff 100%),
+    url("~@/assets/glassbits.jpg"); /* W3C */
+  height: 273px;
+  width: 390px;
+}
+</style>
